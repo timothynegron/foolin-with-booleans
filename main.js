bc1cfd55dc14459fe1f7631e5e0ea5b7caaa764d
Timothy Negron <timothy.negron@codeimmersives.com>
2021-02-19 18:47:58 -0500

    Adds answers. All test passed.

--- main.js
+++ main.js
@@ -2,8 +2,61 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5 (num) {
+  return num > 5;
+}
 
+function isNewTopScore (score, TopScore) {
+  return score > TopScore;
+}
 
+function isInDanger (grade) {
+  return grade >= 60 && grade <= 71;
+}
+
+function isCoasting (grade) {
+  return grade >= 72 && grade <= 83;
+}
+
+function isSucceeding (grade) {
+  return grade >= 84 && grade <= 92;
+}
+
+function isFailing (grade) {
+  return grade < 60;
+}
+
+function isAcing (grade) {
+  return grade > 92;
+}
+
+function isStudent (role) {
+  return role === 'student';
+}
+
+function isTeacher (role) {
+  return role === 'teacher';
+}
+
+function isAdmin (role) {
+  return role === 'admin';
+}
+
+function isElementary (schoolLevel) {
+  return schoolLevel === 'elementary';
+}
+
+function areDifferentPeople (name1, name2) {
+  return name1 !== name2;
+}
+
+function isMiddleSchoolTeacher (role, level) {
+  return role === 'teacher' && level >= 6 && level <= 8;
+}
+
+function notAnElementarySchoolAdministrator (schoolLevel, role) {
+  return !isElementary(schoolLevel) || !isAdmin(role);
+}
 
 
 /********************************************************************************************
